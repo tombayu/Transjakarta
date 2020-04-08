884a0208b7a935e311f95d93dfd16c95d11329f2
tombayu <38533168+tombayu@users.noreply.github.com>
2020-04-08 20:39:24 +0200

    Scrape routes, download all route maps.

--- 01_ScrapeTransjakartaWebsite.R
+++ 01_ScrapeTransjakartaWebsite.R
@@ -1,31 +1,63 @@
 library(rvest)
 library(magrittr)
 library(stringr)
+library(tibble)
+library(dplyr)
+library(readr)
 
 getwd()
 
+# The URL fom which the routes will be scraped from
 tj_website <- xml2::read_html("https://www.transjakarta.co.id/peta-rute/")
 
+### Gather all the map URL
 tj_website %>%
   html_nodes("#peta") %>%
   html_attr("href") %>%
   gsub("\\.\\.", "https://www.transjakarta.co.id", .) -> urls
 
+### Gather all the routes
 tj_website %>%
   html_nodes("#bus-content~ #service-menu .uk-panel-box") %>%
-  html_attrs()
+  html_text() %>%
+  gsub("\\r", "", .) %>%
+  gsub("\\t", "", .) %>%
+  str_split("\\n") %>%
+  unlist() -> r
 
-download.file(urls[[1]], "data/petarute/test1.png", mode = "wb")
+routes <- c()
+for (i in seq_along(r)) {
+  if (grepl(":", r[[i]])) {
+    routes <- c(routes, r[[i]])
+  }
+}
+# There is one temporary route that needs to be added..
+routes <- c(routes[1:33], "TEMP1 : Stasiun Manggarai - Tosari", routes[34:235])
+routes
 
-tj_website %>%
-  html_nodes("#peta") %>%
-  html_attrs()
+rsplt <- strsplit(routes, " : ")
+routesDF <- data.frame("KodeRute" = character(), "NamaRute" = character())
+for (i in seq_along(rsplt)) {
+  routesDF <- routesDF %>%
+    add_row("KodeRute" = ifelse(startsWith(rsplt[[i]][1], "0"), gsub("0", "", rsplt[[i]][1]), rsplt[[i]][1]), "NamaRute" = rsplt[[i]][2])
+}
 
+write_rds(routesDF, "data/rds/routesDF.rds")
 
-?download.file
-urls[[1]]
+### Download route map
+for (i in seq_along(urls)) {
+  cat(i, "\n", routesDF$KodeRute[[i]], "\n")
+  download.file(urls[[i]], paste0("data/petarute/", routesDF$KodeRute[[i]], " ", routesDF$NamaRute[[i]], ".png"), mode = "wb")
+}
 
-paste0()
-urls[[1]]
-gsub("\\.\\.", "", urls[[1]])
-help(rvest)
+KU <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
+
+routesDF
+(dl <- gsub(".png", "", list.files("data/petarute/")))
+(l <- sort(routesDF$KodeRute))
+for (i in seq_along(dl)) {
+  cat(i, "\n")
+  cat(l[[i]], "\n")
+  cat(dl[[i]], "\n")
+  cat(dl[[i]] == l[[i]], "\n")
+}
