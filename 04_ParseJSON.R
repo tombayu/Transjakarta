2b36653a32fa4aa5dfff7f09e304393a25192924
tombayu <38533168+tombayu@users.noreply.github.com>
2020-04-12 17:36:54 +0200

    Cleaner data, updated date
    As of 12/04/2020, Transjakarta operates (a maximum of) 236 corridors.

--- 04_ParseJSON.R
+++ 04_ParseJSON.R
@@ -4,10 +4,10 @@ rm(list = ls())
 
 rute <- read_csv("data/csv/rute03.csv")
 
-TJ <- data.frame(KodeRute = character(), Rute = character(), Layanan = character(), 
-                 JumlahHalte = integer(), Warna = character(), 
-                 TrafiID = character(), API_URL = character())
-# seq_along(rute$KodeRute)
+TJ <- data.frame(kode_rute = character(), rute = character(), layanan = character(), 
+                 n_halte = integer(), warna = character(), 
+                 trafi_id = character(), api_url = character(), tanggal = as.Date(character()))
+
 for (i in seq_along(rute$KodeRute)) {
   cat(paste0(i, ": ", rute$KodeRute[[i]], "\n"))
   t <- try(fromJSON(paste0("data/json/", rute$KodeRute[[i]], ".json")), silent = T)
@@ -15,10 +15,11 @@ for (i in seq_along(rute$KodeRute)) {
     cat(paste0("No json file for ", rute$KodeRute[[i]], "\n"))
   } else {
     k <- fromJSON(paste0("data/json/", rute$KodeRute[[i]], ".json"))
+    url <- rute$API_URL[[i]]
     TJ <- TJ %>%
-      add_row(KodeRute = k$name, Rute = k$longName, Layanan = k$transportName, 
-              JumlahHalte = length(k$stops$id), Warna = k$color, 
-              TrafiID = k$id, API_URL = rute$API_URL[[i]])
+      add_row(kode_rute = k$name, rute = k$longName, layanan = k$transportName, 
+              n_halte = length(k$stops$id), warna = k$color, 
+              trafi_id = k$id, api_url = url, tanggal = Sys.Date())
     stops <- k$stops %>%
       select(-areaName, -directionName)
     write_csv(stops, paste0("data/csv/koridor/", k$name, ".csv"))
