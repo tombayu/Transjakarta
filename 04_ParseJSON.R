65fd79b77a95d4dce962adedf113c2683d0975cb
tombayu <38533168+tombayu@users.noreply.github.com>
2020-04-12 22:39:01 +0200

    W/o purrr

--- 04_ParseJSON.R
+++ 04_ParseJSON.R
@@ -1,12 +1,15 @@
-pacman::p_load(readr, dplyr, magrittr, tibble, jsonlite)
+pacman::p_load(readr, dplyr, magrittr, tibble, jsonlite, purrr)
 
 rm(list = ls())
 
 rute <- read_csv("data/csv/rute03.csv")
 
-TJ <- data.frame(kode_rute = character(), rute = character(), layanan = character(), 
+TJ <- data.frame(kode_koridor = character(), koridor = character(), layanan = character(), 
                  n_halte = integer(), warna = character(), 
-                 trafi_id = character(), api_url = character(), tanggal = as.Date(character()))
+                 trafi_id = character(), api_url = character(), 
+                 #rute = list(), halte = list(),
+                 tanggal = as.Date(character()))
+
 
 for (i in seq_along(rute$KodeRute)) {
   cat(paste0(i, ": ", rute$KodeRute[[i]], "\n"))
@@ -17,9 +20,11 @@ for (i in seq_along(rute$KodeRute)) {
     k <- fromJSON(paste0("data/json/", rute$KodeRute[[i]], ".json"))
     url <- rute$API_URL[[i]]
     TJ <- TJ %>%
-      add_row(kode_rute = k$name, rute = k$longName, layanan = k$transportName, 
+      add_row(kode_koridor = k$name, koridor = k$longName, layanan = k$transportName, 
               n_halte = length(k$stops$id), warna = k$color, 
-              trafi_id = k$id, api_url = url, tanggal = Sys.Date())
+              trafi_id = k$id, api_url = url, 
+              #rute = k$tracks, halte = k$stops,
+              tanggal = Sys.Date())
     stops <- k$stops %>%
       select(-areaName, -directionName)
     write_csv(stops, paste0("data/csv/koridor/", k$name, ".csv"))
@@ -27,3 +32,7 @@ for (i in seq_along(rute$KodeRute)) {
 }
 
 write_csv(TJ, "data/csv/rute04.csv")
+
+dat <- fromJSON("data/json/1.json")
+map(map(dat, "tracks"), "shape")
+dat$tracks$shape
