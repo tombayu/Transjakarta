a213cd6504ebe4cb4872d9c9146302bd1d2bd125
tombayu <38533168+tombayu@users.noreply.github.com>
2020-04-12 23:54:00 +0200

    Utilize purrr to include routes and stops into the DF.

--- 04_ParseJSON.R
+++ 04_ParseJSON.R
@@ -1,4 +1,4 @@
-pacman::p_load(readr, dplyr, magrittr, tibble, jsonlite, purrr)
+pacman::p_load(readr, dplyr, magrittr, tibble, jsonlite, purrr, tidyr)
 
 rm(list = ls())
 
@@ -6,10 +6,8 @@ rute <- read_csv("data/csv/rute03.csv")
 
 TJ <- data.frame(kode_koridor = character(), koridor = character(), layanan = character(), 
                  n_halte = integer(), warna = character(), 
-                 trafi_id = character(), api_url = character(), 
-                 #rute = list(), halte = list(),
-                 tanggal = as.Date(character()))
-
+                 trafi_id = character(), api_url = character(),
+                 tanggal = as.Date(character()), kid = character())
 
 for (i in seq_along(rute$KodeRute)) {
   cat(paste0(i, ": ", rute$KodeRute[[i]], "\n"))
@@ -19,20 +17,23 @@ for (i in seq_along(rute$KodeRute)) {
   } else {
     k <- fromJSON(paste0("data/json/", rute$KodeRute[[i]], ".json"))
     url <- rute$API_URL[[i]]
+    id <- rute$KodeRute[[i]]
     TJ <- TJ %>%
       add_row(kode_koridor = k$name, koridor = k$longName, layanan = k$transportName, 
               n_halte = length(k$stops$id), warna = k$color, 
               trafi_id = k$id, api_url = url, 
-              #rute = k$tracks, halte = k$stops,
-              tanggal = Sys.Date())
-    stops <- k$stops %>%
-      select(-areaName, -directionName)
-    write_csv(stops, paste0("data/csv/koridor/", k$name, ".csv"))
+              tanggal = Sys.Date(), kid = id)
+    # stops <- k$stops %>%
+    #   select(-areaName, -directionName)
+    # write_csv(stops, paste0("data/csv/koridor/", k$name, ".csv"))
   }
 }
 
-write_csv(TJ, "data/csv/rute04.csv")
+write_csv(TJ %>% select(-kid), "data/csv/rute04.csv")
+TJ <- TJ %>%
+  mutate(route_info = map(paste0("data/json/", kid, ".json"), fromJSON))
+TJ <- TJ %>%
+  mutate(rute = map(TJ$route_info, "tracks"),
+         halte = map(TJ$route_info, "stops"))
 
-dat <- fromJSON("data/json/1.json")
-map(map(dat, "tracks"), "shape")
-dat$tracks$shape
+write_rds(TJ, "data/rds/TJ.rds")
